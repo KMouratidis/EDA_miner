a76366d07093367d342d228fcce4291d5dc76752
Konstantinos Mouratidis <k.mouratidis@hotmail.com>
2019-04-17 18:49:52 +0300

    Attempting to make a better baseline, using alchemy & cooking techniques

--- apps/analyze/models/utils.py
+++ apps/analyze/models/utils.py
@@ -1,10 +1,43 @@
 import pandas as pd
 import numpy as np
+import peakutils
+
+def _moving_average(values, roll_window=3, correction=False):
+    values = pd.Series(values)
+    roll = values.rolling(roll_window).mean()
+    roll[:roll_window] = values[:roll_window].mean()
+
+    if correction:
+        correction = int(roll_window / 2)
+        roll = roll.shift(-correction)
+        roll[correction:] = values[correction:]
+
+    return roll
 
 
-def baseline(values, min_max="min", ema_window=7, roll_window=7):
-    """ Numpy implementation of EMA
-    """
+def _exp_moving_average(values, window):
+    """ Numpy implementation of EMA"""
+
+    weights = np.exp(np.linspace(-1., 0.5, window))
+    weights /= weights.sum()
+    a = np.convolve(values, weights, mode='full')[:len(values)]
+    a[:window] = a[window]
+
+    return a
+
+
+def _ema_baseline(values, func, ema_window=7, roll_window=7):
+
+    ema = _exp_moving_average(values, ema_window)
+
+    roll = _moving_average(func([values, ema], axis=0), roll_window)
+    roll = pd.Series(func([values, roll], axis=0))
+
+    return roll
+
+
+def baseline(values, min_max="min", deg=7, ema_window=7, roll_window=7,
+             max_it=200, tol=1e-4):
 
     if min_max == "min":
         bound = np.min
@@ -13,14 +46,12 @@ def baseline(values, min_max="min", ema_window=7, roll_window=7):
     else:
         raise NotImplementedError
 
-    weights = np.exp(np.linspace(-1., 0., ema_window))
-    weights /= weights.sum()
-    ema = np.convolve(values, weights, mode='full')[:len(values)]
-    ema[:ema_window] = ema[ema_window]
+    ema_baseline = _ema_baseline(values, bound, ema_window, roll_window)
+    peakutils_baseline = peakutils.baseline(values, deg=deg,
+                                            max_it=max_it,
+                                            tol=tol)
 
-    roll = pd.Series(bound([values, ema], axis=0))
-    roll = roll.rolling(roll_window).mean()
-    roll[:roll_window] = bound(values[:roll_window])
-    roll = pd.Series(bound([values, ema], axis=0))
+    # Give double the weight to peakutil's values
+    return np.mean([peakutils_baseline * 4/3,
+                    ema_baseline * 2/3], 0)
 
-    return roll
