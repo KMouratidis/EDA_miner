5d0cb847935e7d8cdb7a09ec6198ddd99c99d0bc
Kostas Mouratidis <k.mouratidis@hotmail.com>
2019-09-01 10:24:30 +0300

    Properly initialize project

--- EDA_miner/initialize_project.py
+++ EDA_miner/initialize_project.py
@@ -4,13 +4,17 @@ initialization action needed to fire up the project.
 """
 
 from users_mgt import add_user
-from wsgi import flask_app, db
-from config import env_config_get
+from flask_app import flask_app
+from config import env_config_get, config
+from app_extensions import db
 import os
 
 
 if __name__ == "__main__":
     if not os.path.exists("users.db"):
+
+        flask_app.config.update(config)
+        db.init_app(flask_app)
         with flask_app.app_context() as ctx:
             # Create the database
             db.create_all()
